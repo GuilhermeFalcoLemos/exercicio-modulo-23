3f04d351ea2b0b62d124b3ffa3a139c2b12743b9
GuilhermeFalcoLemos <falco.guilherme@hotmail.com>
2026-08-15 17:03:56 -0300

    feat: adiciona relatorio mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
-  },
+
+    baseUrl: process.env.BASE_URL || "http://localhost:3000"
+  }
 });
